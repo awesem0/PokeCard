cf7f820cba0c286a566d3e99023449266e111a44
awesem0 <adventureidlab@gmail.com>
2025-07-18 18:25:36 +0100

    Fix ESLint max-len error on line 113 in index.js

--- functions/index.js
+++ functions/index.js
@@ -86,7 +86,8 @@ exports.getFirebaseConfig = functions.https.onRequest((req, res) => {
     if (allowedOrigins.includes(origin)) {
         res.set('Access-Control-Allow-Origin', origin);
     } else {
-        res.set('Access-Control-Allow-Origin', 'https://grok-poem-maker-c2ef7.web.app');
+        res.set('Access-Control-Allow-Origin',
+                'https://grok-poem-maker-c2ef7.web.app');
     }
     res.json({
         apiKey: firebaseApiKey.value(),
